07e526e7ae2bfc3a0c4242d1540514d105e603d7
Devin Suttles <devinsuttles@outlook.com>
2017-01-21 14:20:12 -0600

    Update latestShoes.py

--- latestShoes.py
+++ latestShoes.py
@@ -15,6 +15,7 @@ from selenium.webdriver.support import expected_conditions as EC
 chromedriver = "/Users/devinsuttles/Downloads/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chromedriver)
+#New nike men shoes url:http://swoo.sh/2j8FWMT
 url = raw_input("Enter URL:")
 driver.maximize_window()
 driver.get(url)
@@ -23,20 +24,19 @@ wait = WebDriverWait(driver,10)
 #refreshes the page if it's not ressponding
 driver.set_page_load_timeout(30)
 
-#opening size dropdown
-size_button = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".exp-pdp-size-and-quantity-container a.exp-pdp-size-dropdown")))
-actions = ActionChains(driver)
-actions.move_to_element(size_button).click().perform()
-
+def checkout():
+	size_button = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".exp-pdp-size-and-quantity-container a.exp-pdp-size-dropdown")))
+	actions = ActionChains(driver)
+	actions.move_to_element(size_button).click().perform()
 # selecting size
-size = wait.until(EC.visibility_of_element_located((By.XPATH, "//li[contains(@class, 'nsg-form--drop-down--option') and normalize-space(.) = '9.5']")))
-actions = ActionChains(driver)
-actions.move_to_element(size).click().perform()
-
+	size = wait.until(EC.visibility_of_element_located((By.XPATH, "//li[contains(@class, 'nsg-form--drop-down--option') and normalize-space(.) = '9.5']")))
+	actions = ActionChains(driver)
+	actions.move_to_element(size).click().perform()
 #add to cart
-driver.find_element_by_id("buyingtools-add-to-cart-button").click()        
-
+	driver.find_element_by_id("buyingtools-add-to-cart-button").click()
 #checkout
-checkout_button = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".checkout-button")))
-actions = ActionChains(driver)
-actions.move_to_element(checkout_button).click().perform()
+	checkout_button = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".checkout-button")))
+	actions = ActionChains(driver)
+	actions.move_to_element(checkout_button).click().perform()
+
+checkout()
