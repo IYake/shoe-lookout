e913f0ae24dcf8e6b34f0e9107e0bafad3c643b3
Devin Suttles <devinsuttles@outlook.com>
2017-01-21 15:28:32 -0600

    Update and rename latestShoes.py to cart.py

--- cart.py
+++ cart.py
@@ -4,8 +4,6 @@ Created on Tue Oct 25 13:41:09 2016
 
 """
 
-
-
 import os 
 from selenium import webdriver
 from selenium.webdriver import ActionChains
@@ -18,14 +16,14 @@ chromedriver = "/Users/devinsuttles/Downloads/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chromedriver)
 
-driver.maximize_window()
-driver.get("http://swoo.sh/2j8FWMT")
-wait = WebDriverWait(driver,10)
-
-#refreshes the page if it's not ressponding
-driver.set_page_load_timeout(30)
-
 def checkout():
+	driver.maximize_window()
+	driver.get("http://swoo.sh/2j8FWMT")
+	wait = WebDriverWait(driver,10)
+	driver.set_page_load_timeout(30)
+#to look for newest shoe 
+	find = driver.find_element_by_class_name('product-display-name').click()
+#size button 
 	size_button = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".exp-pdp-size-and-quantity-container a.exp-pdp-size-dropdown")))
 	actions = ActionChains(driver)
 	actions.move_to_element(size_button).click().perform()
@@ -39,5 +37,4 @@ def checkout():
 	checkout_button = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".checkout-button")))
 	actions = ActionChains(driver)
 	actions.move_to_element(checkout_button).click().perform()
-
 
