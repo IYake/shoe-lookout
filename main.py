05915763f19b1d08e4758e833e23d0d3cec7e2d3
IYake <iangyake@gmail.com>
2017-01-21 15:39:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+import cart, shoeNotification, shoe_checker
 
+#Check if shoes are correct
+#Go to shoe product page
+#Add shoe to Cart
+#Notify
+correctShoe = "Nike SB Zoom Stefan Janoski Premium High Tape"
+
+count = 0
+shoesMatch = False
+latestShoe = shoe_checker.updateShoe()
+
+while (not shoesMatch):
+    print "checking " + str(count)
+    ##Get the latest shoe and update latestShoe variable
+    if (correctShoe == latestShoe):
+        shoesMatch = True
+    else:
+        count += 1
+        
+    latestShoe = shoe_checker.updateShoe()
+    
+    if count == 50:
+        shoesMatch = True
+        
+cart.checkout()
+
+shoeNotification.notify('iangyake@gmail.com')
