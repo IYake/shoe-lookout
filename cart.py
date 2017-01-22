d89d40c2821c5af583366e11517cd89c483c333e
IYake <iangyake@gmail.com>
2017-01-22 15:32:20 -0600

    Quick fix to cart.py
    Stops the page from loading forever and goes around a bug in EC used in line 34

--- cart.py
+++ cart.py
@@ -1,8 +1,9 @@
 """
 Created on Tue Oct 25 13:41:09 2016
 @author: Devin Suttles
-
 """
+
+
 
 import os 
 from selenium import webdriver
@@ -10,21 +11,32 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 #creating the chrome driver 
-chromedriver = "/Users/devinsuttles/Downloads/chromedriver"
+chromedriver = "/Users/Ian/Downloads/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chromedriver)
 
+
 def checkout():
-	driver.maximize_window()
-	driver.get("http://swoo.sh/2j8FWMT")
-	wait = WebDriverWait(driver,10)
-	driver.set_page_load_timeout(30)
+	#driver.maximize_window()
+	wait = WebDriverWait(driver,5)
+	driver.set_page_load_timeout(5)
+	try:
+		driver.get("http://swoo.sh/2j8FWMT")
+	except TimeoutException:
+		driver.execute_script("window.stop();")
 #to look for newest shoe 
 	driver.find_element_by_class_name('product-display-name').click()
-#size button 
+#size button
+	try: 
+		size_button = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".exp-pdp-size-and-quantity-container a.exp-pdp-size-dropdown")))
+ 	except TimeoutException:
+		driver.execute_script("window.stop();")
+  
 	size_button = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".exp-pdp-size-and-quantity-container a.exp-pdp-size-dropdown")))
+
 	actions = ActionChains(driver)
 	actions.move_to_element(size_button).click().perform()
 # selecting size
@@ -37,4 +49,3 @@ def checkout():
 	checkout_button = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".checkout-button")))
 	actions = ActionChains(driver)
 	actions.move_to_element(checkout_button).click().perform()
-
