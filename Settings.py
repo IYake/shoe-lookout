b012015d072a2b4dc94b8d47b728ff8e3ad22279
Devin Suttles <devinsuttles@outlook.com>
2017-01-26 16:01:11 -0600

    Add files via upload

---
+++ Settings.py
@@ -0,0 +1,9 @@
+""" 
+Created on Thur Jan 26 17:41:09 2017
+@author: Devin Suttles
+
+"""  
+file = open("Nike.txt","r")
+size = file.readline()
+username = file.readline()
+password = file.readline()
